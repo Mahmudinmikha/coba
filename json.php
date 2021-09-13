10970d3d58c76b9ee7eb5e7def8c0a8fd94a54f7
Mahmudinmikha <90544463+Mahmudinmikha@users.noreply.github.com>
2021-09-13 11:56:24 +0700

    Add files via upload

---
+++ json.php
@@ -0,0 +1,22 @@
+<?php
+$array = Array (
+    "0" => Array (
+        "id" => "USR1",
+        "name" => "Steve Jobs",
+        "company" => "Apple"
+    ),
+    "1" => Array (
+        "id" => "USR2",
+        "name" => "Bill Gates",
+        "company" => "Microsoft"
+    ),
+    "2" => Array (
+        "id" => "USR3",
+        "name" => "Mark Zuckerberg",
+        "company" => "Facebook"
+    )
+)
+
+
+
+?>
